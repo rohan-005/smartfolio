48cd16f8be6f6083a4b28bf3d12a8424ee5fa7a4
rohan-005 <rohandhanerwal@gmail.com>
2026-05-11 11:43:18 +0530

    cors

--- backend/server.js
+++ backend/server.js
@@ -26,6 +26,7 @@ app.use(cors({
     origin: [
         'http://localhost:5173',
         'https://smartfolio-nine.vercel.app'
+        
     ],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
